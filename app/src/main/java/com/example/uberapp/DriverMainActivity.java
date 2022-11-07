87d7936c34b0dc8c6038c4445e2296f4ff30fa72
jokicjovan <jokicjovan@outlook.com>
2022-11-07 23:40:09 +0100

    DriverMainActivity added

---
+++ app/src/main/java/com/example/uberapp/DriverMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uberapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_main);
+    }
+}
--- app/src/main/java/com/example/uberapp/LoginActivity.java
+++ app/src/main/java/com/example/uberapp/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.uberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button btn = (Button)findViewById(R.id.loginButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, DriverMainActivity.class));
+            }
+        });
     }
 }
