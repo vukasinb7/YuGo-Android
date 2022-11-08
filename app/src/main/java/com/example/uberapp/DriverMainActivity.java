2d52ebec49cbc6dbd0c941bbc930d7208f6bfecb
Jovan Jokić <jokicjovan@outlook.com>
2022-11-08 19:28:38 +0100

    Driver navigation bar and features fragments added

--- app/src/main/java/com/example/uberapp/DriverMainActivity.java
+++ app/src/main/java/com/example/uberapp/DriverMainActivity.java
@@ -1,14 +1,47 @@
 package com.example.uberapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DriverMainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class DriverMainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.driver_home);
+    }
+    DriverHomeFragment driverHomeFragment = new DriverHomeFragment();
+    DriverChatFragment driverChatFragment = new DriverChatFragment();
+    DriverHistoryFragment driverHistoryFragment = new DriverHistoryFragment();
+    DriverAccountFragment driverAccountFragment = new DriverAccountFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.driver_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, driverHomeFragment).commit();
+                return true;
+            case R.id.driver_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, driverChatFragment).commit();
+                return true;
+            case R.id.driver_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, driverHistoryFragment).commit();
+                return true;
+            case R.id.driver_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, driverAccountFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
