ff00bcc46f7291420861dfdaf4b3c37e84277d7e
ForLoop111 <vlada.devic.2001@gmail.com>
2022-11-08 23:00:08 +0100

    Added: intent on create account button click

--- app/src/main/java/com/example/uberapp/NewUserRegisterActivity.java
+++ app/src/main/java/com/example/uberapp/NewUserRegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.uberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewUserRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class NewUserRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user_register);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonCreateAccount:
+                Intent passengerMainActivity = new Intent(this, PassengerMainActivity.class);
+                startActivity(passengerMainActivity);
+        }
     }
 }
