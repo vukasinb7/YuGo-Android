dadf97e3b434ff0dba3e00f0bd9f636371165c4c
jokicjovan <jokicjovan@outlook.com>
2022-11-09 20:56:42 +0100

    inbox tabs added

--- app/src/main/java/com/example/uberapp/NotificationsFragment.java
+++ app/src/main/java/com/example/uberapp/NotificationsFragment.java
@@ -8,10 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DriverHomeFragment extends Fragment {
+public class NotificationsFragment extends Fragment {
 
-    public DriverHomeFragment() {
-
+    public NotificationsFragment() {
+        // Required empty public constructor
     }
 
     @Override
@@ -23,6 +23,6 @@ public class DriverHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_home, container, false);
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
     }
 }
