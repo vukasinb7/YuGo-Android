358e17ba65b6fc3784b5f603b7d91dba174d2a3d
jokicjovan <jokicjovan@outlook.com>
2022-11-10 22:23:39 +0100

    Updated colors, themes and driver info menu

--- app/src/main/java/com/example/uberapp/home/PassengerCurrentRideFragment.java
+++ app/src/main/java/com/example/uberapp/home/PassengerCurrentRideFragment.java
@@ -1,17 +1,18 @@
 package com.example.uberapp.home;
 
+import android.content.Context;
 import android.os.Bundle;
 
-import androidx.cardview.widget.CardView;
+import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.PopupMenu;
 
 import com.example.uberapp.R;
 
@@ -36,26 +37,20 @@ public class PassengerCurrentRideFragment extends Fragment {
         profilePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RelativeLayout driverPopupMenu = (RelativeLayout) view.findViewById(R.id.driverPopupMenu);
-                if(driverPopupMenu.getVisibility()==View.GONE) {
-                    driverPopupMenu.setVisibility(View.VISIBLE);
-                }
-                else if (driverPopupMenu.getVisibility() == View.VISIBLE)
-                {
-                    driverPopupMenu.setVisibility(View.GONE);
-                }
+                showPopupMenu(v);
             }
         });
 
-        TextView reportDriver = (TextView) view.findViewById(R.id.reportDriver);
-        reportDriver.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         return view;
     }
 
-
+    public void showPopupMenu(View view){
+        Context wrapper = new ContextThemeWrapper(getActivity(), R.style.popupBgStyle);
+        PopupMenu popupMenu = new PopupMenu(wrapper,view);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        Menu menu = popupMenu.getMenu();
+        menu.setGroupDividerEnabled(true);
+        inflater.inflate(R.menu.driver_options_popup_menu, menu);
+        popupMenu.show();
+    }
 }
