0f1d05ad53907908e6b8f42e74728efdaf905c79
jokicjovan <jokicjovan@outlook.com>
2022-11-10 02:08:03 +0100

    Popup menu became clickable

--- app/src/main/java/com/example/uberapp/PassengerHistoryFragment.java
+++ app/src/main/java/com/example/uberapp/PassengerHistoryFragment.java
@@ -8,34 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PassengerHistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PassengerHistoryFragment extends Fragment {
-
 
     public PassengerHistoryFragment() {
         // Required empty public constructor
     }
 
-    public static PassengerHistoryFragment newInstance(String param1, String param2) {
-        PassengerHistoryFragment fragment = new PassengerHistoryFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
--- app/src/main/java/com/example/uberapp/PassengerHomeFragment.java
+++ app/src/main/java/com/example/uberapp/PassengerHomeFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PassengerHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PassengerHomeFragment extends Fragment {
 
 
@@ -20,27 +15,17 @@ public class PassengerHomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public static PassengerHomeFragment newInstance(String param1, String param2) {
-        PassengerHomeFragment fragment = new PassengerHomeFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // redirecting to passenger current ride for now
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.flPassengerMainFragment,  new PassengerCurrentRideFragment()).commit();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_passenger_home, container, false);
     }
