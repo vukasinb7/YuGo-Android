3dbd8f038cb484844c159a6b4172334ae583df03
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-11-11 17:03:56 +0100

    Imported SplashScreen
    Imported SplashScreen, but need to fix zoom to fill on splash screen animation

--- app/src/main/java/com/example/uberapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/uberapp/SplashScreenActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.VideoView;
 
 import com.example.uberapp.account.LoginActivity;
 
@@ -22,6 +23,37 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
                 finish();
             }
-        },1000);
+        },5500);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);  //casting to VideoView is not Strictly required above API level 26
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.splashscreen);
+        setDimension(videoView);//set the path of the video that we need to use in our VideoView
+        videoView.start();  //start() method of the VideoView class will start the video to play
+
+    }
+    private void setDimension(VideoView videoView) {
+        // Adjust the size of the video
+        // so it fits on the screen
+        float videoProportion = getVideoProportion();
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        int screenHeight = getResources().getDisplayMetrics().heightPixels;
+        float screenProportion = (float) screenHeight / (float) screenWidth;
+        android.view.ViewGroup.LayoutParams lp = videoView.getLayoutParams();
+
+        if (videoProportion < screenProportion) {
+            lp.height= screenHeight;
+            lp.width = (int) ((float) screenHeight / videoProportion);
+        } else {
+            lp.width = screenWidth;
+            lp.height = (int) ((float) screenWidth * videoProportion);
+        }
+        videoView.setLayoutParams(lp);
+    }
+
+    // This method gets the proportion of the video that you want to display.
+// I already know this ratio since my video is hardcoded, you can get the
+// height and width of your video and appropriately generate  the proportion
+//    as :height/width
+    private float getVideoProportion(){
+        return 1.5f;
     }
 }
