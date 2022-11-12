ab0e63e77cfdeb5611aceb1eda48b847e2768390
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-11-12 17:38:03 +0100

    Minor Design Fixes

--- app/src/main/java/com/example/uberapp/gui/home/CreateRideFragment.java
+++ app/src/main/java/com/example/uberapp/gui/home/CreateRideFragment.java
@@ -1,5 +1,7 @@
 package com.example.uberapp.gui.home;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
