0a1a2328a0b16980f8ad3df45a338c54ed4dc200
ForLoop111 <vlada.devic.2001@gmail.com>
2022-11-12 12:49:24 +0100

    refactor

--- app/src/main/java/com/example/uberapp/core/tools/Mockup.java
+++ app/src/main/java/com/example/uberapp/core/tools/Mockup.java
@@ -16,6 +16,10 @@ public class Mockup {
         Bitmap icon1 = BitmapFactory.decodeFile("https://github.com/vukasinb7/UberApp/blob/5d17a2ef3d3fcb9315bc11ea12e3c4268b85ec96/app/src/main/res/drawable/car_model.png");
         VehicleType vt1 = new VehicleType("001", 5.0, VehicleCategory.Standard, icon1);
 
+        Bitmap icon2 = BitmapFactory.decodeFile("https://github.com/vukasinb7/UberApp/blob/d17b7e32840871afd5715d8b28a05d962ac88db6/app/src/debug/res/drawable/car_model_02.png");
+        VehicleType vt2 = new VehicleType("002", 7.5, VehicleCategory.Lux, icon2);
+        
+
         vehicleTypes.add(vt1);
         return vehicleTypes;
     }
