e1fc3ec94cfa46df1a67e752e44f0e994406371c
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2023-01-12 20:01:42 +0100

    Implemented Maps

--- app/src/main/java/com/example/uberapp/gui/fragments/home/DriverCurrentRideFragment.java
+++ app/src/main/java/com/example/uberapp/gui/fragments/home/DriverCurrentRideFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,12 +17,21 @@ import android.widget.PopupMenu;
 
 import com.example.uberapp.R;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.MapTileIndex;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
 
 public class DriverCurrentRideFragment extends Fragment {
 
     public DriverCurrentRideFragment() {
         // Required empty public constructor
     }
+    MapView map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,7 +43,34 @@ public class DriverCurrentRideFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_driver_current_ride, container, false);
+        Context ctx = getActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        map = (MapView) view.findViewById(R.id.mapView);
+        map.getTileProvider().clearTileCache();
+        Configuration.getInstance().setCacheMapTileCount((short)12);
+        Configuration.getInstance().setCacheMapTileOvershoot((short)12);
+        // Create a custom tile source
+        map.setTileSource(new OnlineTileSourceBase("", 1, 20, 512, ".png",
+                new String[] { "https://a.tile.openstreetmap.org/" }) {
+            @Override
+            public String getTileURLString(long pMapTileIndex) {
+                return getBaseUrl()
+                        + MapTileIndex.getZoom(pMapTileIndex)
+                        + "/" + MapTileIndex.getX(pMapTileIndex)
+                        + "/" + MapTileIndex.getY(pMapTileIndex)
+                        + mImageFilenameEnding;
+            }
+        });
 
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        GeoPoint startPoint;
+        startPoint = new GeoPoint(51.0, 4.0);
+        mapController.setZoom(11.0);
+        mapController.setCenter(startPoint);
+        final Context context = this.getContext();
+        map.invalidate();
+        createmarker();
         ImageView profilePic = (ImageView) view.findViewById(R.id.passengerProfilePic);
         profilePic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +80,19 @@ public class DriverCurrentRideFragment extends Fragment {
         });
 
         return view;
+    }
+    public void createmarker(){
+        if(map == null) {
+            return;
+        }
+
+        Marker my_marker = new Marker(map);
+        my_marker.setPosition(new GeoPoint(4.1,51.1));
+        my_marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        my_marker.setTitle("Give it a title");
+        my_marker.setPanToView(true);
+        map.getOverlays().add(my_marker);
+        map.invalidate();
     }
 
 
