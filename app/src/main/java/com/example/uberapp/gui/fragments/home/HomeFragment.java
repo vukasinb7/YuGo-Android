af245356fb5c51ed9ecd0df601e1bc955050932c
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2023-01-15 23:06:05 +0100

    Minor Fix

--- app/src/main/java/com/example/uberapp/gui/fragments/home/HomeFragment.java
+++ app/src/main/java/com/example/uberapp/gui/fragments/home/HomeFragment.java
@@ -204,6 +204,8 @@ public class HomeFragment extends Fragment implements CurrentRideFragment.OnEndC
                     mapFragment.createRoute(departure.getLatitude(), departure.getLongitude(),
                             destination.getLatitude(), destination.getLongitude());
 
+                    nextRide = null;
+
                 }
 
                 @Override
@@ -227,6 +229,7 @@ public class HomeFragment extends Fragment implements CurrentRideFragment.OnEndC
         if(nextRide != null){
             startRideButton.setVisibility(View.VISIBLE);
         }
+
         if(TokenManager.getRole().equals("DRIVER"))
             mapFragment = MapFragment.newInstance(false);
         else
