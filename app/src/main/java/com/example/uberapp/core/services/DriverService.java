c78a85c720844337adf9b5271a9a9d6a9af8babc
ForLoop111 <vlada.devic.2001@gmail.com>
2023-01-27 20:16:19 +0100

    missing import

--- app/src/main/java/com/example/uberapp/core/services/DriverService.java
+++ app/src/main/java/com/example/uberapp/core/services/DriverService.java
@@ -1,11 +1,10 @@
 package com.example.uberapp.core.services;
 
 import com.example.uberapp.core.dto.AllRidesDTO;
+import com.example.uberapp.core.dto.DocumentDTO;
 import com.example.uberapp.core.dto.StatisticsDTO;
 import com.example.uberapp.core.dto.UserDetailedDTO;
 import com.example.uberapp.core.dto.VehicleDTO;
-
-import java.util.List;
 
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
